857e5d9c618d282495bc1f1fc87f364cde9d08ae
srushs5699 <srushtishinde12@gmailcom>
2025-10-25 17:26:43 -0700

    directory fix

--- event-ticketing-system/backend/src/main/java/com/example/ticketing/dto/AuthRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.ticketing.dto;
-
-import lombok.Data;
-
-@Data
-public class AuthRequest {
-    private String username;
-    private String password;
-}
--- event-ticketing-system/backend/src/main/java/com/example/ticketing/repository/EventRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ticketing.repository;
-
-import com.example.ticketing.model.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventRepository extends JpaRepository<Event, Long> {
-}
